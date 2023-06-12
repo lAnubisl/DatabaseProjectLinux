fdbd9b028dc828262996b0884ca0ba300a29f8e4
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2023-06-12 18:23:09 +0000

    Initial commit

---
+++ create_database_project.sh
@@ -0,0 +1,7 @@
+# Install project template
+dotnet new -i Microsoft.Build.Sql.Templates
+
+# Create new Database Project
+dotnet new sqlproj -n MyDatabaseProject
+
+# Then make changes in the database project
