a7669b05090fe8753d1d398498c3cff78b2c2158
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2023-06-17 14:55:46 +0000

    remove unused files

--- MyDatabaseProject/Security/func-dotnet-db-test.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER [func-dotnet-db-test] from external provider with DEFAULT_SCHEMA = [xyz];
-GO;
-GRANT DELETE,INSERT,SELECT,UPDATE ON SCHEMA::[xyz] TO [func-dotnet-db-test];
-GO;
-GRANT UNMASK TO [func-dotnet-db-test];
-GO;
