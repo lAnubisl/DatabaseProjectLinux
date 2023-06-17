cdfaf2c872d9314d3a908661b6c76998fb675bfe
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2023-06-17 14:57:58 +0000

    remove unused files

--- create_database_project.sh
+++
@@ -1,7 +0,0 @@
-# Install project template
-dotnet new -i Microsoft.Build.Sql.Templates
-
-# Create new Database Project
-dotnet new sqlproj -n MyDatabaseProject
-
-# Then make changes in the database project
